60f61d357e28b04c81ae2dbf41ccca3791a4896c
fangping <fangpinga@163.com>
2017-11-01 08:12:32 +0800

    eureka 注册中心学习

---
+++ cloud_eureka_server/src/main/java/com/moa/cloud/CloudEurekaServerApplication.java
@@ -0,0 +1,12 @@
+package com.moa.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudEurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudEurekaServerApplication.class, args);
+	}
+}
