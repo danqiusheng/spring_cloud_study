43235e82087d124936fb08e1aadd5a816d2b3538
fangping <fangpinga@163.com>
2017-11-08 08:45:03 +0800

    zuul 学习

---
+++ cloud_config_client/src/main/java/com/moa/cloud/web/TestController.java
@@ -0,0 +1,28 @@
+package com.moa.cloud.web;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by Administrator on 2017/11/7.
+ */
+
+//这个注解不明白
+@RefreshScope
+@RestController
+public class TestController {
+
+    private String name;
+
+
+    @Autowired
+    private Environment environment;
+
+    @RequestMapping("/from")
+    public String from(){
+        return environment.getProperty("from","default");
+    }
+}
